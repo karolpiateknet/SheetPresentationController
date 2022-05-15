676e60849f313980ac601d974cab497475e66844
Karol Piątek <karolpiateknet@users.noreply.github.com>
2022-05-15 16:56:18 +0200

    Make container properties configurable

--- Classes/SheetPresentationContainerView.swift
+++ Classes/SheetPresentationContainerView.swift
@@ -9,19 +9,44 @@ import UIKit
 
 public final class SheetPresentationContainerView: UIView {
     private enum Constants {
-        static let scrollIndicatorHeight: CGFloat = 5
-        static let cornerRadius: CGFloat = 48
+        static let defaultScrollIndicatorHeight: CGFloat = 5
+        static let defaultScrollIndicatorWidth: CGFloat = 36
+        static let defaultCornerRadius: CGFloat = 48
     }
 
-    public var topIndicatorColor: UIColor? {
+    var cornerRadius: CGFloat = Constants.defaultCornerRadius {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            topIndicatorToContainerConstraint?.constant = cornerRadius / 2
+        }
+    }
+
+    var topIndicatorColor: UIColor? {
         get { topIndicator.backgroundColor }
         set { topIndicator.backgroundColor = newValue }
     }
 
+    var topIndicatorHeight: CGFloat = Constants.defaultScrollIndicatorHeight {
+        didSet {
+            topIndicatorHeightConstraint?.constant = topIndicatorHeight
+            topIndicator.layer.cornerRadius = topIndicatorHeight / 2
+        }
+    }
+
+    var topIndicatorWidth: CGFloat = Constants.defaultScrollIndicatorWidth {
+        didSet {
+            topIndicatorWidthConstraint?.constant = topIndicatorWidth
+        }
+    }
+
+    private var topIndicatorHeightConstraint: NSLayoutConstraint?
+    private var topIndicatorWidthConstraint: NSLayoutConstraint?
+    private var topIndicatorToContainerConstraint: NSLayoutConstraint?
+
     private let topIndicator: UIView = {
         let view = UIView()
         view.backgroundColor = .gray
-        view.layer.cornerRadius = Constants.scrollIndicatorHeight / 2
+        view.layer.cornerRadius = Constants.defaultScrollIndicatorHeight / 2
         view.layer.masksToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -61,15 +86,23 @@ private extension SheetPresentationContainerView {
         addSubview(topIndicator)
         addSubview(container)
         translatesAutoresizingMaskIntoConstraints = false
+
+        let topIndicatorHeightConstraint = topIndicator.heightAnchor.constraint(equalToConstant: topIndicatorHeight)
+        let topIndicatorWidthConstraint = topIndicator.widthAnchor.constraint(equalToConstant: topIndicatorWidth)
+        self.topIndicatorHeightConstraint = topIndicatorHeightConstraint
+        self.topIndicatorWidthConstraint = topIndicatorHeightConstraint
+
         NSLayoutConstraint.activate([
             topIndicator.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
-            topIndicator.widthAnchor.constraint(equalToConstant: 36),
-            topIndicator.heightAnchor.constraint(equalToConstant: Constants.scrollIndicatorHeight),
+            topIndicatorWidthConstraint,
+            topIndicatorHeightConstraint,
             topIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor)
         ])
 
+        let topIndicatorToContainerConstraint = container.topAnchor.constraint(equalTo: topIndicator.bottomAnchor, constant: cornerRadius / 2)
+        self.topIndicatorToContainerConstraint = topIndicatorToContainerConstraint
         NSLayoutConstraint.activate([
-            container.topAnchor.constraint(equalTo: topIndicator.bottomAnchor, constant: 24),
+            topIndicatorToContainerConstraint,
             container.leadingAnchor.constraint(equalTo: leadingAnchor),
             container.trailingAnchor.constraint(equalTo: trailingAnchor),
             container.bottomAnchor.constraint(equalTo: bottomAnchor)
@@ -79,7 +112,7 @@ private extension SheetPresentationContainerView {
     func setupUI() {
         backgroundColor = .white
         layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        layer.cornerRadius = Constants.cornerRadius
+        layer.cornerRadius = Constants.defaultCornerRadius
         layer.masksToBounds = false
         clipsToBounds = true
     }
